78054cbdfcdc3e23c6319ed079f4a45b3ab11ae9
hufu_vendor <hufu_vendor@sensetime.com>
2019-06-25 19:58:18 +0800

    整理图片

--- src/views/elementCom/eltable.vue
+++ src/views/elementCom/eltable.vue
@@ -1,46 +1,64 @@
 <template>
     <div class="eltable">
-        <el-table
-            :data="tableData.slice((currpage - 1) * pagesize, currpage * pagesize)"
-            ref="multipleTable"
-            tooltip-effect="dark"
-            @selection-change="handleSelectionChange"
-            :row-key="getRowKeys"
-            style="width: 100%">
-            <el-table-column
-                type="selection"
-                :reserve-selection="true"
-                width="55">
-            </el-table-column>
-            <el-table-column
-                label="日期"
-                width="120"
-                property="dataid">
-                <template slot-scope="scope">{{ scope.row.date }}</template>
-            </el-table-column>
-            <el-table-column
-                prop="name"
-                label="姓名"
-                width="120">
-            </el-table-column>
-            <el-table-column
-                prop="address"
-                label="地址"
-                show-overflow-tooltip>
-            </el-table-column>
-        </el-table>
-        <el-pagination
-            background
-            :page-size='pagesize'
-            layout="prev, pager, next"
-            @current-change="handleCurrentChange"
-            :total="tableData.length">
-        </el-pagination>
+		<el-button type="primary" @click="seenTable=true">查看表格</el-button>
+		<el-button type="primary" @click="getClubPictures">获取壁纸</el-button>
+        <div class="table" v-show="seenTable">
+            <el-table
+                :data="tableData.slice((currpage - 1) * pagesize, currpage * pagesize)"
+                ref="multipleTable"
+                tooltip-effect="dark"
+                @selection-change="handleSelectionChange"
+                :row-key="getRowKeys"
+                style="width: 100%">
+                <el-table-column
+                    type="selection"
+                    :reserve-selection="true"
+                    width="55">
+                </el-table-column>
+                <el-table-column
+                    label="日期"
+                    width="120"
+                    property="dataid">
+                    <template slot-scope="scope">{{ scope.row.date }}</template>
+                </el-table-column>
+                <el-table-column
+                    prop="name"
+                    label="姓名"
+                    width="120">
+                </el-table-column>
+                <el-table-column
+                    prop="address"
+                    label="地址"
+                    show-overflow-tooltip>
+                </el-table-column>
+            </el-table>
+            <el-pagination
+                background
+                :page-size='pagesize'
+                layout="prev, pager, next"
+                @current-change="handleCurrentChange"
+                :total="tableData.length">
+            </el-pagination>
+        </div>
+        <div v-if="seen">
+			<ul  class="ul-api" v-for="item in pictures.slice((currPicPage - 1) * pagesize, currPicPage * pagesize)" :key="item.index">
+				<li><img :src="item" width="800" height="600"></li>
+			</ul>
+            <el-pagination
+                background
+                :page-size='pagesize'
+                layout="prev, pager, next"
+                @current-change="handleCurrPicChange"
+                :total="pictures.length">
+            </el-pagination>
+		</div>
+        
     </div>
 </template>
 
 <script>
     //import  from ''
+	import apiService from '@/services/API-service.js'
 
     export default {
         //组件名
@@ -76,10 +94,14 @@
                     date: '2016-05-07',
                     name: '王小虎',
                     address: '上海市普陀区金沙江路 1518 弄'
-                    }],
-                    multipleSelection: [],
-                    currpage: 1,
-                    pagesize: 5
+                }],
+                multipleSelection: [],
+                currpage: 1,
+                pagesize: 5,
+                seen:false,
+                pictures:[],
+                currPicPage: 1,
+                seenTable:false,
             }
         },
         //数组或对象，用于接收来自父组件的数据
@@ -100,7 +122,19 @@
             },
             getRowKeys(row) {
                 return row.date;
-            }
+            },
+            getClubPictures() {
+				this.seen = !this.seen
+				apiService.TestURL.acgclubURL().then(res=>{
+					let {data} = res;
+					data.forEach(item => {
+						this.pictures.push(item.thumbnail);
+					});
+				}).catch()
+            },
+            handleCurrPicChange(cpage) {
+                this.currPicPage = cpage;
+            },
         },
         //生命周期函数
         created() {
