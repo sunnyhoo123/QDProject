d3235114d3911ab5807cf20cc67a1d8948c1f221
sunnyhoo123 <sunnyhoo2015@gmail.com>
2020-12-30 22:39:41 +0800

    fix: 解决冲突

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -59,68 +59,7 @@ export default {
     },
   },
   methods: {
-<<<<<<< HEAD
     
-=======
-    getApiData() {
-      // 未封装请求方法
-      // axios.post('http://test.admin.broker.hm.com:10010/exchange_api/base/listFiatCurrency', {id: 2}
-      // ).then(function (rep) {
-      // console.log(rep)
-      // })
-
-      // 将参数封装到API-config.js中，将方法封装到API-service.js中
-      apiService.TestURL.imgTestURL().then(res => {
-        this.getResult = res.results
-      }).catch()
-    },
-    getMockdata() {
-      // axios.get("http://localhost:8080/static/mock/alarmList.json").then(res => {
-      api.get("/static/mock/alarmList.json").then(res => {
-        this.responseData = cloneDeep(res)
-        this.responseData.image.url = "www"
-        console.log(res, "res"); // 使用cloneDeep，赋值后res不会随之改变
-        console.log(this.responseData, "responseData");
-      });
-    },
-    changeBg() {
-      let r = Math.floor((Math.random() * 10))
-      console.log(r)
-      if(this.seen) {
-        this.$refs.element.style.background = "url(" + require("@/assets/image/home/demo" + r + ".jpg") + ")no-repeat"
-      }
-    },
-    hiddenBG() {
-      this.seen = !this.seen
-      if (this.seen) {
-        this.hiddenBg = "隐藏图片"
-      } else {
-        this.hiddenBg = "显示图片"
-      }
-    },
-    openMenu(e) {
-      const menuMinWidth = 105;
-      const offsetLeft = this.$el.getBoundingClientRect().left
-      const offsetWidth = this.$el.offsetWidth // container width
-      const maxLeft = offsetWidth - menuMinWidth // left boundary
-      const left = e.clientX - offsetLeft // 15: margin right
-      
-      if (left > maxLeft) {
-        this.left = maxLeft
-      } else {
-        this.left = left
-      }
-
-      this.top = e.clientY - 60 // fix 位置bug
-      this.visible = true
-    },
-    closeMenu() {
-      this.visible = false
-    },
-    // change() {
-    //   this.isCollapse = !this.isCollapse;
-    // }
->>>>>>> eaab615206ff0feaff1926eabf399601eb0829e7
   },
 }
 </script>
@@ -138,65 +77,7 @@ export default {
   .menuCollapse {
     margin-left: 64px;
   }
-<<<<<<< HEAD
   .main-right {
-=======
-}
-.test {
-  height: 100vh
-}
-  h1,h2 {
-    font-weight: normal;
-  }
-  /* [title]{
-    background: #42b983
-  } */
-  .ul-cont {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  .li-cont {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
-
-  .background{
-    width: 900px;
-    height: 500px;
-    background: url('~@/assets/image/home/demo1.jpg') no-repeat
-  }
-  .contract{
-    display: flex;
-  }
-  .up{
-    width: 7px;
-    height: 7px;
-    border-top: 2px solid rgb(129,180,229);
-    border-left: 2px solid rgb(129,180,229);
-    transform: rotate(45deg);
-  }
-  .icon-wallet{
-    width: 20px;
-    background: red;
-  }
-  .ul-api{
-    li{
-      float: left;
-    }
-  }
-  .modules{
-    display: flex;
-  }
-  .contextmenu {
-    margin: 0;
-    background: #fff;
-    z-index: 3000;
->>>>>>> eaab615206ff0feaff1926eabf399601eb0829e7
     position: absolute;
     top: 8px;
     right: 8px;
