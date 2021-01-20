2216c4abc96f61167560fa2089f539523117052f
sunnyhoo123 <sunnyhoo2015@gmail.com>
2021-01-20 09:16:41 +0800

    删除dist文件夹

--- dist/static/js/30.7127cf7292c121c693bf.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([30],{MUOe:function(e,t){},YSFz:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("el-button",{attrs:{plain:!0},on:{click:this.open}},[this._v("打开消息提示")])],1)},staticRenderFns:[]};var i=n("VU/8")({name:"ElMessageCom",data:function(){return{}},created:function(){},methods:{open:function(){this.$message("这是一条消息提示")}}},s,!1,function(e){n("MUOe")},null,null);t.default=i.exports}});
-//# sourceMappingURL=30.7127cf7292c121c693bf.js.map
