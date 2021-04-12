d5fd4ff03e0a11a3ed06aae380387c6fc40da214
sunnyhoo123 <sunnyhoo2015@gmail.com>
2021-04-12 22:30:20 +0800

    新增setvisiable

--- src/views/Feature/components/zTree.vue
+++ src/views/Feature/components/zTree.vue
@@ -37,36 +37,47 @@ export default {
   methods: {
     filterTree() {
       console.log( 88);
-      // this.filterTreeList = _.cloneDeep(this.treeObj.transformToArray(this.treeObj.getNodes()));
-      // console.log(this.filterTreeList, 88);
-      // const reg = new RegExp(`(${this.inputText.replace(/([/*.\\])/g, "\\$1")})`, "ig");
-      // const matcher = (str) => reg.test(str);
-      // const highlight = (str) => str.replace(reg, `<span style="color:#fc652f;margin:0">$1</span>`);
-      // const filter = (nodes) => {
-      //   nodes.forEach(node => {
-      //     const { main, sub, name, code, children } = node;
-      //     // let main = main;
-      //     // let sub = sub;
-      //     if(matcher(name) || matcher(sub)) {
-      //       // if(!node.isHidden) {
-      //       //   this.setVisible(node.getPath(), true);
-      //       // }
-      //       this.setVisible(node.getPath(), true);
-      //     }else {
-      //       this.setVisible([node], false);
-      //     }
-      //     // node.fullName = `${highlight(main)}`;
-      //     node.name = `${highlight(name)}`;
-      //     this.treeObj.updateNode(node, false);
-      //     if(children && children.length) {
-      //       filter(node.children);
-      //     }
-      //   })
-      // }
-      // // filter(this.treeObj.transformToZtreeNodes(this.filterTreeList.filter(i => i.isHidden === false)));
-      // console.log(this.treeObj, 900)
-      // filter(this.treeObj.transformTozTreeNodes(this.filterTreeList));
-      // this.treeObj.expendAll(true);
+      this.filterTreeList = _.cloneDeep(this.treeObj.transformToArray(this.treeObj.getNodes()));
+      console.log(this.filterTreeList, 88);
+      const reg = new RegExp(`(${this.inputText.replace(/([/*.\\])/g, "\\$1")})`, "ig");
+      const matcher = (str) => reg.test(str);
+      const highlight = (str) => str.replace(reg, `<span style="color:#fc652f;margin:0">$1</span>`);
+      const filter = (nodes) => {
+        nodes.forEach(node => {
+          const { main, sub, name, code, children } = node;
+          // let main = main;
+          // let sub = sub;
+          if(matcher(name) || matcher(sub)) {
+            // if(!node.isHidden) {
+            //   this.setVisible(node.getPath(), true);
+            // }
+            this.setVisible(node.getPath(), true);
+          }else {
+            this.setVisible([node], false);
+          }
+          // node.fullName = `${highlight(main)}`;
+          node.name = `${highlight(name)}`;
+          this.treeObj.updateNode(node, false);
+          if(children && children.length) {
+            filter(node.children);
+          }
+        })
+      }
+      // filter(this.treeObj.transformToZtreeNodes(this.filterTreeList.filter(i => i.isHidden === false)));
+      console.log(this.treeObj, 900)
+      filter(this.treeObj.transformTozTreeNodes(this.filterTreeList));
+      this.treeObj.expendAll(true);
+    },
+    setVisible(nodes, visible) {
+      nodes.forEach(node => {
+        const dom = $(`#${node.tId}`);
+        node.isHidden = !visible;
+        if(visible) {
+          dom.show();
+        } else {
+          dom.hide();
+        }
+      })
     }
   }
 }
